b4736077db026418864263ab82fa3bff1e10c603
maximilian1064 <lei.yan@rwth-aachen.de>
2017-01-04 11:32:29 +0100

    back to editing on hector

--- bench/bin/HBWmembench.sh
+++ bench/bin/HBWmembench.sh
@@ -2,4 +2,14 @@
 #Benchmarking script for HBW memory
 #Lei Yan 01/04/2017
 
+# init ICC and ENVs
+source $iccvar intel64
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+export OMP_NUM_THREADS=32
+
+# benchmarking
+enableHbw="numactl -m 4,5,6,7"
+
+${enableHbw} ../distgen -t 16 -1000
 
